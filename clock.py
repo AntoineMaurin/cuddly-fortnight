614c72e8f84fd6dc097b4cb8c9c1966f5dedbaa8
antoi <antoine.maurin26@gmail.com>
2025-11-09 19:47:41 +0100

    inital commit

---
+++ clock.py
@@ -0,0 +1,19 @@
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
+
+def main(minutes: int = 15):
+    start = time.time()
+    delta_2 = -1
+    while True:  # Press Ctrl+F8 to toggle the breakpoint.
+        delta = time.time() - start
+        if delta_2 != -1:
+            if int(delta) != int(delta_2):
+                print(delta)
+        if delta > minutes * 60:
+            break
+
+        delta_2 = delta
+
+if __name__ == '__main__':
+    main(1)
