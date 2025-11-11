970c94338464913aab72bdf9d9e520f254f3d52d
AntoineMaurin <antoine.maurin26@gmail.com>
2025-11-11 21:54:28 +0100

    wip

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Digits
+import time
+
+from textual.app import App, ComposeResult, RenderResult
+from textual.widgets import Footer, Header, Digits, Input
 from textual.containers import Center
 
-from clock import run
+from textual.widget import Widget
+from textual.reactive import reactive
+
+MINUTES = 0.1
+
+class Hello(Widget):
+
+    name = "base name"
+
+    def render(self) -> RenderResult:
+        return f"Hello, [b]{self.name}[/b]!"
+
+
+class TextInput(Input):
+    placeholder = "write something"
+    name = "input placeholder"
+
+    def on_input_changed(self, c):
+        Hello.name = self.value
 
 class MyApp(App):
     """A Textual app to manage stopwatches."""
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
+    CSS = """
+    Screen {
+        align: center middle;       
+    }
+    
+    #center {
+        width: 120;
+    }
+    """
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
+        self.title = "Tamada"
         yield Header(show_clock=True, name="header name")
-        yield Footer(name="foota")
-        self.title="test title"
-        yield  Digits("3.141,592,653,5897", id="pi")
-        # yield Center(name="Center name")
+        # with Center(id="center"):
+        yield TextInput()
+        yield Hello()
+
+        # yield Footer(name="foota")
 
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
